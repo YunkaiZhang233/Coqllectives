17775832395adb8b4d5253b66ce0608797eaf7aa
Yunkai Zhang <yunkai.zhang22@imperial.ac.uk>
2024-07-29 17:18:17 +0800

    completed lookup to assoc soundness proof

--- stlc.v
+++ stlc.v
@@ -7,6 +7,17 @@ Local Open Scope string_scope.
   For example the definition of type might look like 
 *)
 
+Ltac solve_by_inverts n :=
+  match goal with | H : ?T |- _ =>
+  match type of T with Prop =>
+    solve [
+      inversion H;
+      match n with S (S (?n')) => subst; solve_by_inverts (S n') end ]
+  end end.
+
+Ltac solve_by_invert :=
+  solve_by_inverts 1.
+
 Inductive type : Set :=
   | var_type : string -> type
   | fun_type : type -> type -> type
@@ -51,7 +62,7 @@ Defined. *)
 
 Inductive assoc {A B: Set}: list (A * B) -> A -> B -> Prop :=
   | assoc_front (rest: list (A * B)) (e1 : A) (e2 : B) : assoc (cons (e1, e2) rest) e1 e2
-  | assoc_cons (rest: list (A * B)) (e1 : A) (e2 : B) (f1 : A) (f2 : B) : assoc rest e1 e2 -> assoc (cons (f1, f2) rest) e1 e2
+  | assoc_cons (rest: list (A * B)) (e1 : A) (e2 : B) (f1 : A) (f2 : B) : assoc rest e1 e2 -> e1 <> f1 -> assoc (cons (f1, f2) rest) e1 e2
   .
 
 Print sig.
@@ -84,8 +95,8 @@ corresponds to the derivability of the judgment
 Inductive has_type: list (string * type) -> term -> type -> Prop :=
   | has_type_var (gamma: list (string * type)) (x : string) (a : type): 
       assoc (cons (x, a) gamma) x a -> has_type gamma (var_term x) a
-  | has_type_abs (gamma: list (string * type)) (x : string) (a : type) (m : term) (b : type):
-      assoc (cons (x, a) gamma) x a -> has_type gamma m b -> has_type gamma (abs_term x a m) (fun_type a b)
+  | has_type_abs (gamma: list (string * type)) (x : string) (a : type) (m : term) (b : type) (ft : type):
+      assoc (cons (x, a) gamma) x a -> has_type gamma m b -> ft = fun_type a b ->  has_type gamma (abs_term x a m) ft
   | has_type_app (gamma: list (string * type)) (a b : type) (f n : term):
       has_type gamma f (fun_type a b) -> has_type gamma n a -> has_type gamma (app_term f n) b
   .
@@ -125,9 +136,76 @@ Fixpoint type_check
       end
   end.
 
+Search sumbool.
+
+
+
 
 Definition typeA := var_type "A".
 Definition varX := var_term "x".
 
+Lemma lookup_front_diff : 
+  forall (x' : string) (t' : type) (l : list (string * type)) (x : string) (t : type),
+  x <> x' ->
+  lookup string_dec ((x', t') :: l) x = lookup string_dec l x.
+Proof.
+  intros x' t' l x t Hneq.
+  elim (string_dec x x'); intros Hcon.
+  unfold lookup.
+  - (* This case is not possible due to assumption Hneq *)
+    exfalso.
+    apply Hneq.
+    assumption.
+  - (* x <> x' *)
+    simpl.
+    destruct (string_dec x' x) eqn:H.
+    + exfalso. auto.
+    + simpl.
+      auto.
+Qed.
+
 Definition empty_env : list (string * type) := nil.
 Compute (type_check (cons ("x", typeA) empty_env) varX).
+
+Search "if" sumbool.
+
+Theorem lookup_assoc (context : list (string * type)) (x : string) (t : type) :
+  @lookup string type string_dec context x = Some t -> assoc context x t.
+Proof.
+  intros Hl.
+  induction context as [| h l IH].
+  (* base case *)
+  - exfalso.
+    simpl in Hl.
+    discriminate Hl.
+  (* inductive case *)
+  - destruct h  as [x' t'].
+    destruct (string_dec x x') as [Heq | Hneq].
+    (* x = x' *)
+    + rewrite Heq in Hl.
+      rewrite Heq in IH. 
+      rewrite Heq.
+      simpl in Hl.
+      destruct (string_dec x' x') eqn:con.
+      {
+        inversion Hl.
+        apply assoc_front.
+      }
+      {
+        contradiction.
+      }
+
+    (* x <> x'*)
+    + apply assoc_cons; auto.
+      apply IH.
+      apply eq_trans with (y:=lookup string_dec ((x', t') :: l) x).
+      symmetry.
+      apply lookup_front_diff; auto.
+      exact Hl.
+Qed.
+
+Theorem typecheck_safe (context : list (string * type)) (m : term) (t : type):
+  type_check context m = Some t -> has_type context m t.
+Proof.
+
+Qed.
